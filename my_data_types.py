007969f01487b0064f868024e9c3261d9340f43f
StormH <sebel@ukr.net>
2022-10-21 10:37:10 +0300

    my first homework

---
+++ my_data_types.py
@@ -0,0 +1,50 @@
+my_int_var = 10
+my_str_var = "hello world"
+print(my_str_var, my_int_var)
+
+my_int_var = 5
+my_str_var = "how are you ?"
+print(type(my_str_var), type(my_int_var))
+print(my_str_var,my_int_var)
+
+x = int(5)
+print(type(x))
+
+x = str("Hello World")
+print(type(x))
+
+x = int(20)
+print(type(x))
+
+money_rate = 30
+money = int(20)
+grand_money = money_rate * money
+print(type(grand_money))
+
+number_a = 15+15
+number_b = number_a
+print(number_b)
+
+a = str(type(10))
+print(a)
+
+my_result = "how are you"
+my_name = "Sergey"
+print(my_result, my_name)
+
+x = "Hello my friend"
+print(x,type(x))
+
+a = 'how are you'
+b = 'not bad'
+c = 'good'
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+
+a = 15+15
+b = 'to much'
+print(a,type(a), b,type(b))
+
+
+
