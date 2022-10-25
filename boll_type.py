aad937cf51d286cdb611b7a77285c445972a69fd
StormH <sebel@ukr.net>
2022-10-25 17:47:50 +0300

    my first homework

---
+++ boll_type.py
@@ -0,0 +1,28 @@
+
+comparison_result: bool = 3 != 5
+print(comparison_result)
+
+comparison_result: bool = 5 <= 5
+print(comparison_result)
+
+comparison_result: bool = 5 >= 5
+print(comparison_result)
+
+comparison_result: bool = 5 == 5
+print(comparison_result)
+
+is_true = True or True and False
+print(is_true)
+
+is_true = True and True or False
+print(is_true)
+
+is_true = True and True, not False
+print(is_true)
+
+is_true = True or True or False
+print(is_true)
+
+is_true = True or True, not False
+print(is_true)
+bool(10)
