a7cfac5c932ce6c33e6f7a036bd7506d91bc5756
StormH <sebel@ukr.net>
2022-10-27 09:16:44 +0300

    my first homework

--- boll_type.py
+++ boll_type.py
@@ -1,4 +1,3 @@
-
 comparison_result: bool = 3 != 5
 print(comparison_result)
 
@@ -17,12 +16,19 @@ print(is_true)
 is_true = True and True or False
 print(is_true)
 
-is_true = True and True, not False
-print(is_true)
-
 is_true = True or True or False
 print(is_true)
 
-is_true = True or True, not False
+is_true = True or not True or False
 print(is_true)
 
+is_true = True and True or not False
+print(is_true)
+
+print(bool(None) == bool(7))
+
+print(bool(" ") == bool(10 - 1))
+
+print(bool('Hello') == bool(True or False))
+
+print(type(bool(None)) == id(bool(None)))
