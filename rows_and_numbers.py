cb8fd154f98acf0613ec2b08588d7825c96466fc
StormH <sebel@ukr.net>
2022-10-29 06:31:18 +0300

    my first homework

---
+++ rows_and_numbers.py
@@ -0,0 +1,15 @@
+username = input("Please tell me your name")
+result = f"Hello {username}, nice to e-meet you!"
+
+print(result)
+
+username = input("Please  write a small number")
+result = f" {username} "
+
+print(result)
+
+result = f"int{username}"
+print(result)
+print(
+
+)
