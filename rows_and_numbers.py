b59c5c530a90071ab2dd0eb97cc6f19655e58173
StormH <sebel@ukr.net>
2022-10-29 07:40:58 +0300

    my first homework

--- rows_and_numbers.py
+++ rows_and_numbers.py
@@ -3,13 +3,19 @@ result = f"Hello {username}, nice to e-meet you!"
 
 print(result)
 
-username = input("Please  write a small number")
-result = f" {username} "
+y = input("Please give me a number fraction")
+y_1 = f"{y}"
+print(y_1,type(y_1))
 
-print(result)
+x= int(5.9)
+print(x,type(x))
 
-result = f"int{username}"
-print(result)
-print(
+x_1 = 10 ** 4
+print(x_1,type(x_1))
 
-)
+x_2 = 16 ** 0.5
+print(x_2,type(x_2))
+
+x_3 = 5 % 3
+print(x_3,type(x_3))
+
