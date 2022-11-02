1408bcc9881aa1d5b36ab891b595b9a79c0b040f
StormH <sebel@ukr.net>
2022-11-02 20:51:18 +0200

    my first homework

---
+++ flow_control.py
@@ -0,0 +1,33 @@
+number = float(input("enter the number"))
+number_2 = int(input(" enter the second number"))
+
+print('{} + {} = '.format(number, number_2))
+print(number + number_2)
+
+print('{} - {} = '.format(number, number_2))
+print(number - number_2)
+
+print('{} * {} = '.format(number, number_2))
+print(number * number_2)
+
+print('{} ** {} = '.format(number, number_2))
+print(number ** number_2)
+
+print('{} / {} = '.format(number, number_2))
+
+if number_2 == 0:
+    print('cannot divide by zero')
+else:
+    print(number / number_2)
+
+print(type(number))
+print(type(number_2))
+
+a = number or number_2
+if a < 100:
+    print('Low')
+elif 0 <= a <= 100:
+    print('Mid')
+else:
+    print('High')
+
