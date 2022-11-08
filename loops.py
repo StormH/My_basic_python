818ff56e13ab57eebbe47b6c094b3803ee07e6e0
StormH <sebel@ukr.net>
2022-11-08 22:15:16 +0200

    my first homework

---
+++ loops.py
@@ -0,0 +1,22 @@
+# txt = input("enter the words")
+#
+# for upper, in txt:
+#     if upper.isupper():
+#         print(upper)
+#     continue
+#
+# for index, element in enumerate(txt):
+#     if element.isspace():
+#         print(f"{index=}|{element}")
+#     continue
+#
+# for las in txt:
+#     if las in "aeiouAEIOU":
+#         print(las)
+
+# digit = input("something")
+#
+# for num in digit:
+#     if num.isdigit():
+#         break
+#     print(num, "alarm")
