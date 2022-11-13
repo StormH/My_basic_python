eb9b829105b2434f8ddc5215779601d2eda0dc25
StormH <sebel@ukr.net>
2022-11-13 10:34:13 +0200

    homework 5

--- loops.py
+++ loops.py
@@ -26,26 +26,36 @@ for num in digit:
 else:
     print("all ok")
 ##########################################
-while True:
-    choice = input("enter  (+, -, *, /)")
-    if choice == '0':
-        print('wrong input')
-    if choice == "exit":
-        break
-    if choice in ('+', '-', '*', '/'):
-        x = float(input("enter number 1:"))
-        y = float(input("enter number 2:"))
-
-        if choice == '+':
-            print("result:", (x + y))
-        elif choice == '-':
-            print("result:", (x - y))
-        elif choice == '*':
-            print("result:", (x * y))
-        elif choice == '/':
-            if y != 0:
-                print("result:", (x / y))
-            else:
-                print("divided by zero!")
-    else:
-        print("Wrong input")
+# while True:
+#     choice = input("enter  (+, -, *, /)")
+#     if choice == '0':
+#         print('wrong input')
+#     if choice == "exit":
+#         break
+#     if choice in ('+', '-', '*', '/'):
+#         x = input("enter number 1:")
+#
+#         try:
+#             x = int(x)
+#         except:
+#             x = float(x)
+#         y = input("enter number 2:")
+#
+#         try:
+#             y = int(y)
+#         except:
+#             y = float(y)
+#
+#         if choice == '+':
+#             print("result:", (x + y), type(x+y))
+#         elif choice == '-':
+#             print("result:", (x - y), type(x-y))
+#         elif choice == '*':
+#             print("result:", (x * y), type(x*y))
+#         elif choice == '/':
+#             if y != 0:
+#                 print("result:", (x / y), type(x / y))
+#             else:
+#                 print("divided by zero!")
+#     else:
+#         print("Wrong input")
