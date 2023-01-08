5dc1ed08098befd617e4fe14c8b90607c1ea9379
StormH <sebel@ukr.net>
2023-01-08 13:40:13 +0200

    homework 8,9,10

---
+++ virtualenv_practice.py
@@ -0,0 +1,48 @@
+# 1. in the terminal made virtual environment and activation
+# 2. after activation can open my file in the folder my_first_script.py and get data " Hello world"
+# 3. after that via "pip install" installed few modules
+# 4. in use " pip freeze" get full list of installed modules
+# 5, finally make deactivation.
+
+# c:\>c:\code\Envs\Scripts\activate.bat
+#
+# (Envs) c:\>c:\code\Envs\Scripts\activate.bat
+#
+# (Envs) c:\>c:\code\projects\my_basic_python\my_first_script.py
+# Hello World
+# (Envs) c:\>deactivate
+# c:\>
+#
+# (my_cool_env) c:\>pip freeze
+# anyio==3.6.2
+# appdirs==1.4.4
+# beautifulsoup4==4.11.1
+# bs4==0.0.1
+# certifi==2022.12.7
+# charset-normalizer==2.1.1
+# contourpy==1.0.6
+# cycler==0.11.0
+# fonttools==4.38.0
+# h11==0.14.0
+# haversine==2.7.0
+# httpcore==0.16.3
+# httpx==0.23.3
+# idna==3.4
+# kiwisolver==1.4.4
+# lxml==4.9.2
+# matplotlib==3.6.2
+# nautical==4.1.1
+# numpy==1.24.1
+# packaging==22.0
+# Pillow==9.4.0
+# pykml==0.2.0
+# pyparsing==3.0.9
+# python-dateutil==2.8.2
+# python-telegram-bot==20.0
+# PyYAML==6.0
+# requests==2.28.1
+# rfc3986==1.5.0
+# six==1.16.0
+# sniffio==1.3.0
+# soupsieve==2.3.2.post1
+# urllib3==1.26.13
