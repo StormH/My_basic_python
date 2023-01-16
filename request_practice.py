14716c9b48851b7de94350f36fa5be1049598f6b
StormH <sebel@ukr.net>
2023-01-16 17:55:43 +0200

    homework 14

---
+++ request_practice.py
@@ -0,0 +1,105 @@
+
+# POST 	Создает новый ресурс. 	requests.post()
+# GET 	Считывает имеющийся ресурс. 	requests.get()
+# PUT 	Обновляет существующий ресурс. 	requests.put()
+# DELETE 	Удаляет ресурс. 	requests.delete()
+#####################################################################################
+
+# import requests
+#
+# response = requests.get('https://google.com')
+# print(f"{response.status_code=}")
+#
+# import requests#
+# response = requests.request(url='https://google.com', method="GET")
+#
+# print(f"{response.content=}")
+
+####################################################################################
+# GET
+
+# import requests
+#
+# url = 'https://google.com'
+#
+# response = requests.get(url)
+#
+# print(response)
+# print(response.status_code)
+# print(response.text)
+# print(response.url)
+# print(response.headers['Content-Type'])
+# print(response.content)
+
+#################################################################################
+#POST
+#
+# import requests
+# import json
+#
+# url = 'https://jsonplaceholder.typicode.com/posts'
+#
+# data = { 'title': 'buy new mouse','body': 'I need a new mouse !','userId': 5,}
+# headers = {'content-type': 'application/json; charset=UTF-8'}
+#
+# response = requests.post(url, data=json.dumps(data), headers=headers)
+#
+# print(response.status_code)
+# print(response.ok)
+# print(response.content)
+# print(response.text)
+# print(type(response.text))
+# print(response.url)
+# print(response.headers['Content-Type'])
+# print(response.encoding)
+
+####################################################################################
+
+#PUT
+# import requests
+# import json
+#
+# url = 'https://jsonplaceholder.typicode.com/posts/1'
+# data = {'id':1, 'userId':2, 'title':'drink water', 'body':'drinking water is important'}
+# headers = {'Content-Type':'application/json; charset=UTF-8'}
+# response = requests.put(url, data=json.dumps(data), headers=headers)
+#
+# print(response.status_code)
+# print(response.ok)
+# print(response.content)
+# print(response.text)
+# print(type(response.text))
+# print(response.url)
+# print(response.headers['Content-Type'])
+# print(response.encoding)
+
+#########################################################################################
+
+#DELETE
+# import requests
+# import json
+#
+# url = 'https://jsonplaceholder.typicode.com/posts/1'
+#
+# headers = {'Content-Type': 'application/json; charset=UTF-8'}
+#
+# response = requests.delete(url, headers=headers)
+# print(response.status_code)
+# print(response.ok)
+# print(type(response.text))
+# print(response.url)
+# print(response.headers['Content-Type'])
+# print(response.encoding)
+###############################################################################
+# import requests
+# image = requests.get('https://pngimg.com/uploads/smiley/smiley_PNG27.png')
+# with open('new_image.png', 'wb') as f:
+#  f.write(image.content)
+
+f = open("LICENSE")
+
+print(f)
+print(f.read)
+print(f.readline)
+print(f.close)
+
